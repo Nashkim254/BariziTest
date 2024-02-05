e21989b8e4b1463101bab653af81f43786769931
programming6131 <kimutainashon@gmail.com>
2024-02-05 13:50:31 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.barizi_nashon_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:barizi_nashon_test/app/app.dart';
+import 'package:barizi_nashon_test/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
